840f8b5ebbee12d5c9321eba34944f2a27944103
Tushar Agarwal <tushar7250@gmail.com>
2024-01-22 14:23:19 +0530

    Add files via upload

--- pendo.js
+++ pendo.js
@@ -1,5 +1,6 @@
 <script>
-    (function(apiKey){
+setTimeout(function(){
+  (function(apiKey){
       (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
         v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
           o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
@@ -12,4 +13,5 @@
 
       });
     })('97a73a75-ecf5-49e0-59a5-3a28041d9508');
+}, 3000);
   </script>
